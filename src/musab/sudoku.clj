b8f68fca4062ce68ac664b1320bab644dfaebb21
Musab <musabnazir@gmail.com>
2020-11-09 21:29:36 -0500

    WIP algorithm optimization

--- src/musab/sudoku.clj
+++ src/musab/sudoku.clj
@@ -59,6 +59,58 @@
              row-range)]
     (into [] box)))
 
+(def box-cords
+  {:1 [1 1]
+   :2 [1 4]
+   :3 [1 7]
+   :4 [4 1]
+   :5 [4 4]
+   :6 [4 7]
+   :7 [7 1]
+   :8 [7 4]
+   :9 [7 7]})
+
+(defn count-row-num
+  [row-vals]
+  (if (not-any? zero? row-vals) 0 (count (filter (comp not zero?) row-vals))))
+
+(defn get-start-block
+  [board-state]
+  (let [first-box (count-row-num (get-box-values (:1 box-cords) board-state))
+        second-box (count-row-num (get-box-values (:2 box-cords) board-state))
+        third-box (count-row-num (get-box-values (:3 box-cords) board-state))
+        fourth-box (count-row-num (get-box-values (:4 box-cords) board-state))
+        fifth-box (count-row-num (get-box-values (:5 box-cords) board-state))
+        six-box (count-row-num (get-box-values (:6 box-cords) board-state))
+        seven-box (count-row-num (get-box-values (:7 box-cords) board-state))
+        eight-box (count-row-num (get-box-values (:8 box-cords) board-state))
+        nine-box (count-row-num(get-box-values (:9 box-cords) board-state))
+        max-num (max first-box second-box third-box fourth-box
+                     fifth-box six-box seven-box eight-box nine-box)]
+    (some (fn [[key val]]
+            (and (=
+                  (count
+                   (filter
+                    (comp not zero?)
+                    (get-box-values val board-state))) max-num) (key box-cords)))
+          box-cords)))
+
+(defn get-zero-in-block
+  [board-state]
+  (let [[row col] (get-start-block board-state)
+        vals (get-box-values [row col] board-state)
+        blank-index (first (keep-indexed (fn [idx v] (if (= 0 v) idx)) vals))]
+    (cond 
+      (= blank-index 0) [row col]
+      (= blank-index 1) [row (inc col)]
+      (= blank-index 2) [row (+ 2 col)]
+      (= blank-index 3) [(inc row) col]
+      (= blank-index 4) [(inc row) (inc col)]
+      (= blank-index 5) [(inc row) (+ 2 col)]
+      (= blank-index 6) [(+ 2 row) col]
+      (= blank-index 7) [(+ 2 row) (inc col)]
+      (= blank-index 8) [(+ 2 row) (+ 2 col)])))
+
 (defn get-valid-nums
   "Given a row and col number returns a set of valid nums 
    for that position if that position is free else returns 0"
@@ -95,11 +147,22 @@
                       col (range 9)]
                   [row col])))))
 
+(def recur-count (atom 0))
+(def reset-count (atom 0))
+(reset! recur-count 0)
+(reset! reset-count 0)
+
 (defn solve [board-state]
-  (let [[row col] (get-first-blank-coords board-state)]
+  (let [[row col] (get-zero-in-block board-state)]
     ;; if no more 0 on the board we have a solution
     ;; else we continue recursion
     (if (or (nil? row) (nil? col))
-      (to-array board-state)
-      (flatten (mapv #(solve (assoc-in board-state [row col] %))
-            (get-valid-nums [(inc row) (inc col)] board-state))))))
+      {:solution (to-array board-state)
+       :iter @recur-count
+       :resets @reset-count}
+      (do 
+        (swap! recur-count inc)
+        (if (empty? (get-valid-nums [row col] board-state))
+          (swap! reset-count inc))
+        (flatten (mapv #(solve (assoc-in board-state [(- row 1) (- col 1)] %))
+            (get-valid-nums [row col] board-state)))))))
